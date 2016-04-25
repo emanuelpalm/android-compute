6a7cab0f584ddcbe09b6e6b13a35b33e1a1d604e
Emanuel Palm <emanuelpalm@gmail.com>
2016-04-25 17:30:29 +0200

    Fix spelling mistake in ComputeLambda

--- core/src/main/java/se/ltu/emapal/compute/ComputeLambda.kt
+++ core/src/main/java/se/ltu/emapal/compute/ComputeLambda.kt
@@ -3,10 +3,10 @@ package se.ltu.emapal.compute
 /**
  * A lambda, containing some Lua program able to process arbitrary byte array batches.
  *
- * @param lamdaId Lambda identifier unique within the application.
+ * @param lambdaId Lambda identifier unique within the application.
  * @param program Lua program.
  */
 class ComputeLambda(
-        val lamdaId: Int,
+        val lambdaId: Int,
         val program: String
 )
