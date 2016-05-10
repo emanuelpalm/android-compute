5680dbec0d75f410e6aef5ce4d9d236820cfc81e
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-10 16:22:23 +0200

    Add unit test to TestComputeContext

--- android/src/androidTest/java/se/ltu/emapal/compute/TestComputeContext.kt
+++ android/src/androidTest/java/se/ltu/emapal/compute/TestComputeContext.kt
@@ -27,4 +27,22 @@ class TestComputeContext {
             is Result.Failure -> Assert.fail(processResult.error.message)
         }
     }
+
+    @Test
+    fun shouldRegisterBadLambdaAndReturnError() {
+        val computeContext = ComputeContext()
+
+        var registerResult0 = computeContext.register(ComputeLambda(1, "donut()"))
+        when (registerResult0) {
+            is Result.Success -> Assert.fail("Expected error not returned.")
+            is Result.Failure -> Assert.assertNotEquals(0, registerResult0.error.code)
+        }
+
+        var registerResult1 = computeContext.register(ComputeLambda(1, "Å^()["))
+        when (registerResult1) {
+            is Result.Success -> Assert.fail("Expected error not returned.")
+            is Result.Failure -> Assert.assertNotEquals(0, registerResult1.error.code)
+        }
+    }
+
 }
