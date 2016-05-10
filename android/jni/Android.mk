c2ba2975262a7990db01ebc5b306c74fa25c4d21
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-10 12:33:44 +0200

    Fix mistakes in jni/Android.mk

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -1,14 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE           := libluacompute
-LOCAL_STATIC_LIBRARIES := libs/$(TARGET_ARCH_ABI)/libluacompute.a
-include $(BUILD_STATIC_LIBRARY)
+LOCAL_MODULE     := libluacompute
+LOCAL_SRC_FILES  := libs/$(TARGET_ARCH_ABI)/libluacompute.a
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE           := libluajit-5.1
-LOCAL_STATIC_LIBRARIES := libs/$(TARGET_ARCH_ABI)/libluajit-5.1.a
-include $(BUILD_STATIC_LIBRARY)
+LOCAL_MODULE     := libluajit-5.1
+LOCAL_SRC_FILES  := libs/$(TARGET_ARCH_ABI)/libluajit-5.1.a
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE     := compute
