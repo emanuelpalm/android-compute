e537a85348ba7a1289f10d0942858b900918ae46
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-13 17:31:24 +0200

    Add ViewConnector and use in ActivityMain

--- android/src/main/java/se/ltu/emapal/compute/client/android/ActivityMain.kt
+++ android/src/main/java/se/ltu/emapal/compute/client/android/ActivityMain.kt
@@ -2,14 +2,16 @@ package se.ltu.emapal.compute.client.android
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
-import se.ltu.emapal.compute.client.android.R
 
 class ActivityMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        text_hello.text = "Hello!"
+        view_connector.onConnect.subscribe { uri ->
+            Snackbar.make(view_connector, uri, Snackbar.LENGTH_LONG).show()
+        }
     }
 }
---
+++ android/src/main/java/se/ltu/emapal/compute/client/android/view/ViewConnector.kt
@@ -0,0 +1,50 @@
+package se.ltu.emapal.compute.client.android.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.view_connector.view.*
+import rx.Observable
+import rx.subjects.PublishSubject
+import se.ltu.emapal.compute.client.android.R
+
+/**
+ * Code-behind for [R.layout.view_connector].
+ */
+class ViewConnector : LinearLayout {
+    /** Context constructor. */
+    constructor(context: Context) : super(context)
+
+    /** Context/attribute constructor. */
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    /** Context/attribute/style constructor. */
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private val onConnectSubject = PublishSubject.create<String>()
+
+    /** Fires event containing URI field value whenever the connect button is clicked. */
+    val onConnect: Observable<String>
+        get() = onConnectSubject
+
+    init {
+        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE).let {
+            it as LayoutInflater
+            it.inflate(R.layout.view_connector, this, true)
+        }
+        text_uri.setOnKeyListener { view, code, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && code == KeyEvent.KEYCODE_ENTER) {
+                onConnectSubject.onNext((view as EditText).text.toString())
+                true
+            } else {
+                false
+            }
+        }
+        button_connect.setOnClickListener {
+            onConnectSubject.onNext(text_uri.text.toString())
+        }
+    }
+}
