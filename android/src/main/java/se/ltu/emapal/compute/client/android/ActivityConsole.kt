ebab1cd7c0f99dd570261ca7e90559266a5091e4
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-13 17:31:47 +0200

    Add empty ActivityConsole

---
+++ android/src/main/java/se/ltu/emapal/compute/client/android/ActivityConsole.kt
@@ -0,0 +1,12 @@
+package se.ltu.emapal.compute.client.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityConsole : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_console)
+    }
+}
