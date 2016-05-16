79a6fce9393464492b7dd6f66f6076956065e2e6
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-16 20:25:32 +0200

    Add #applyError() and #mapError to Result

--- core/src/main/java/se/ltu/emapal/compute/util/Result.kt
+++ core/src/main/java/se/ltu/emapal/compute/util/Result.kt
@@ -8,11 +8,13 @@ package se.ltu.emapal.compute.util
  * [Result] type is constructed either as a success, wrapping an arbitrary value, or a failure,
  * wrapping an arbitrary error, and can later be destructed using a when expression.
  */
-sealed class Result<out T, E : Throwable> {
+sealed class Result<T, E : Throwable> {
     /** A successful result. */
     class Success<T, E : Throwable>(val value: T) : Result<T, E>() {
         override fun <U> apply(f: (T) -> Result<U, E>) = f(value)
-        override fun <U> map(f: (T) -> U): Result<U, E> = Result.Success(f(value))
+        override fun <F : Throwable> applyError(f: (E) -> Result<T, F>) = Result.Success<T, F>(value)
+        override fun <U> map(f: (T) -> U) = Result.Success<U, E>(f(value))
+        override fun <F : Throwable> mapError(f: (E) -> F) = Result.Success<T, F>(value)
         override fun unwrap() = value
 
         override fun equals(other: Any?): Boolean {
@@ -32,8 +34,10 @@ sealed class Result<out T, E : Throwable> {
 
     /** A failed result. */
     class Failure<T, E : Throwable>(val error: E) : Result<T, E>() {
-        override fun <U> apply(f: (T) -> Result<U, E>): Result<U, E> = Result.Failure(error)
-        override fun <U> map(f: (T) -> U): Result<U, E> = Result.Failure(error)
+        override fun <U> apply(f: (T) -> Result<U, E>) = Result.Failure<U, E>(error)
+        override fun <F : Throwable> applyError(f: (E) -> Result<T, F>) = f(error)
+        override fun <U> map(f: (T) -> U) = Result.Failure<U, E>(error)
+        override fun <F : Throwable> mapError(f: (E) -> F) = Result.Failure<T, F>(f(error))
         override fun unwrap() = throw error
 
         override fun equals(other: Any?): Boolean {
@@ -54,8 +58,14 @@ sealed class Result<out T, E : Throwable> {
     /** Applies function [f] to value, if a value is available. */
     abstract fun <U> apply(f: (T) -> Result<U, E>): Result<U, E>
 
+    /** Applies function [f] to error, if an error is available. */
+    abstract fun <F : Throwable> applyError(f: (E) -> Result<T, F>): Result<T, F>
+
     /** Maps function [f] to value, if a value is available. */
     abstract fun <U> map(f: (T) -> U): Result<U, E>
+
+    /** Maps function [f] to error, if an error is available. */
+    abstract fun <F : Throwable> mapError(f: (E) -> F): Result<T, F>
 
     /** Unwraps result, causing either its value to be returned, or error to be thrown. */
     abstract fun unwrap(): T
