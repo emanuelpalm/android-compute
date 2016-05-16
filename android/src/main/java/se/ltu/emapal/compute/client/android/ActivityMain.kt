93df525ade27454fcd9af33fff5802d908918f84
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-16 09:53:54 +0200

    Make ViewConnector handle state changes

--- android/src/main/java/se/ltu/emapal/compute/client/android/ActivityMain.kt
+++ android/src/main/java/se/ltu/emapal/compute/client/android/ActivityMain.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import se.ltu.emapal.compute.client.android.view.ViewConnector
 
 class ActivityMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,21 @@ class ActivityMain : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         view_connector.onConnect.subscribe { uri ->
-            Snackbar.make(view_connector, uri, Snackbar.LENGTH_LONG).show()
+            Snackbar.make(
+                    view_connector,
+                    applicationContext.getString(R.string.text_connected_to_s, uri),
+                    Snackbar.LENGTH_LONG
+            ).show()
+            view_connector.setState(ViewConnector.State.SHOW_DISCONNECT)
+        }
+
+        view_connector.onDisconnect.subscribe { uri ->
+            Snackbar.make(
+                    view_connector,
+                    applicationContext.getString(R.string.text_disconnected_from_s, uri),
+                    Snackbar.LENGTH_LONG
+            ).show()
+            view_connector.setState(ViewConnector.State.SHOW_CONNECT)
         }
     }
 }
--- android/src/main/java/se/ltu/emapal/compute/client/android/view/ViewConnector.kt
+++ android/src/main/java/se/ltu/emapal/compute/client/android/view/ViewConnector.kt
@@ -4,12 +4,12 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.KeyEvent
 import android.view.LayoutInflater
-import android.widget.EditText
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.view_connector.view.*
 import rx.Observable
 import rx.lang.kotlin.PublishSubject
 import rx.subjects.PublishSubject
+import rx.subjects.ReplaySubject
 import se.ltu.emapal.compute.client.android.R
 
 /**
@@ -26,26 +26,81 @@ class ViewConnector : LinearLayout {
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     private val onConnectSubject: PublishSubject<String>? = if (isInEditMode) null else PublishSubject()
+    private val onDisconnectSubject: PublishSubject<String>? = if (isInEditMode) null else PublishSubject()
+    private val onStateChangeSubject: ReplaySubject<State>? = if (isInEditMode) null else ReplaySubject.createWithSize(1)
 
     /** Fires event containing URI field value whenever the connect button is clicked. */
     val onConnect: Observable<String>
         get() = onConnectSubject ?: Observable.create { }
+
+    /** Fires event containing URI field value whenever the disconnect button is clicked. */
+    val onDisconnect: Observable<String>
+        get() = onDisconnectSubject ?: Observable.create { }
+
+    /** Fires current state and any subsequent [ViewConnector.State] changes. */
+    val onStateChange: Observable<State>
+        get() = onStateChangeSubject ?: Observable.create { }
 
     init {
         context.getSystemService(Context.LAYOUT_INFLATER_SERVICE).let {
             it as LayoutInflater
             it.inflate(R.layout.view_connector, this, true)
         }
-        text_uri.setOnKeyListener { view, code, event ->
+        field_uri.setOnKeyListener { view, code, event ->
             if (event.action == KeyEvent.ACTION_DOWN && code == KeyEvent.KEYCODE_ENTER) {
-                onConnectSubject?.onNext((view as EditText).text.toString())
+                triggerAction()
                 true
             } else {
                 false
             }
         }
-        button_connect.setOnClickListener {
-            onConnectSubject?.onNext(text_uri.text.toString())
+        button_connect.setOnClickListener { triggerAction() }
+
+        onStateChangeSubject?.onNext(State.SHOW_CONNECT)
+        onStateChange
+                .subscribe { state ->
+                    when (state) {
+                        State.SHOW_CONNECT -> {
+                            text_connect_title.visibility = VISIBLE
+                            text_connect_description.visibility = VISIBLE
+                            field_uri.isEnabled = true
+                            button_connect.isEnabled = true
+                            button_connect.text = context.getText(R.string.action_connect)
+                        }
+                        State.SHOW_CONNECTING -> {
+                            field_uri.isEnabled = false
+                            button_connect.isEnabled = false
+                        }
+                        State.SHOW_DISCONNECT -> {
+                            text_connect_title.visibility = GONE
+                            text_connect_description.visibility = GONE
+                            field_uri.isEnabled = false
+                            button_connect.isEnabled = true
+                            button_connect.text = context.getText(R.string.action_disconnect)
+                        }
+                        else -> throw IllegalStateException("Bad ViewConnector state: $state.")
+                    }
+                }
+    }
+
+    private fun triggerAction() {
+        when (onStateChangeSubject?.value) {
+            State.SHOW_CONNECT -> onConnectSubject?.onNext(field_uri.text.toString())
+            State.SHOW_DISCONNECT -> onDisconnectSubject?.onNext(field_uri.text.toString())
+            else -> throw IllegalStateException("Action triggered while in ${onStateChangeSubject?.value} state.")
         }
+    }
+
+    fun setState(state: State) {
+        onStateChangeSubject?.onNext(state)
+    }
+
+    /**
+     * [ViewConnector] state.
+     */
+    enum class State {
+        SHOW_CONNECT,
+        SHOW_CONNECTING,
+        SHOW_DISCONNECT
     }
 }
