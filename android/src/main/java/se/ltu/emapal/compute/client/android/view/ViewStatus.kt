069424b9daca5114916456b30837118338f9c02a
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-16 14:53:53 +0200

    Make ViewStatus display some statistics

--- android/src/main/java/se/ltu/emapal/compute/client/android/view/ViewStatus.kt
+++ android/src/main/java/se/ltu/emapal/compute/client/android/view/ViewStatus.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.view_status.view.*
+import rx.lang.kotlin.PublishSubject
 import se.ltu.emapal.compute.client.android.R
 
 class ViewStatus : LinearLayout {
@@ -16,10 +18,36 @@ class ViewStatus : LinearLayout {
     /** Context/attribute/style constructor. */
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
+    /** Allows getting and settings [ViewStatus] properties. */
+    val subject = if (!isInEditMode) Subject() else null
+
     init {
         context.getSystemService(Context.LAYOUT_INFLATER_SERVICE).let {
             it as LayoutInflater
             it.inflate(R.layout.view_status, this, true)
         }
+
+        subject?.let {
+            it.whenLambdaCount.subscribe {
+                text_status_lambda_count.text = it.toString()
+            }
+            it.whenPendingBatchCount.subscribe {
+                text_status_pending_batch_count.text = it.toString()
+            }
+            it.whenProcessedBatchCount.subscribe {
+                text_status_processed_batch_count.text = it.toString()
+            }
+        }
+    }
+
+    class Subject {
+        /** Registered lambda count. */
+        val whenLambdaCount = PublishSubject<Int>()
+
+        /** Pending batch count. */
+        val whenPendingBatchCount = PublishSubject<Int>()
+
+        /** Processed batch count. */
+        val whenProcessedBatchCount = PublishSubject<Int>()
     }
 }
