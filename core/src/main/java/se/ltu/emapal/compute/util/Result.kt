c9bea6a4e85fcaa0a8237059f4bc79c11e92aca9
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-16 11:41:32 +0200

    Tidy up in Result.kt

--- core/src/main/java/se/ltu/emapal/compute/util/Result.kt
+++ core/src/main/java/se/ltu/emapal/compute/util/Result.kt
@@ -8,20 +8,19 @@ package se.ltu.emapal.compute.util
  * [Result] type is constructed either as a success, wrapping an arbitrary value, or a failure,
  * wrapping an arbitrary error, and can later be destructed using a when expression.
  */
-@Suppress("unused")
 sealed class Result<out T, E : Throwable> {
     /** A successful result. */
     class Success<T, E : Throwable>(val value: T) : Result<T, E>() {
-        override fun <U> apply(f: (T) -> Result<U, E>): Result<U, E> = f(value)
+        override fun <U> apply(f: (T) -> Result<U, E>) = f(value)
         override fun <U> map(f: (T) -> U): Result<U, E> = Result.Success(f(value))
-        override fun unwrap(): T = value
+        override fun unwrap() = value
     }
 
     /** A failed result. */
     class Failure<T, E : Throwable>(val error: E) : Result<T, E>() {
         override fun <U> apply(f: (T) -> Result<U, E>): Result<U, E> = Result.Failure(error)
         override fun <U> map(f: (T) -> U): Result<U, E> = Result.Failure(error)
-        override fun unwrap(): T = throw error
+        override fun unwrap() = throw error
     }
 
     /** Applies function [f] to value, if a value is available. */
