726f1345920a7e677fedcc678e3a08bbeaca5e47
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-17 16:24:59 +0200

    Make vars into vals in TestComputeContext

--- android/src/androidTest/java/se/ltu/emapal/compute/TestComputeContext.kt
+++ android/src/androidTest/java/se/ltu/emapal/compute/TestComputeContext.kt
@@ -33,13 +33,13 @@ class TestComputeContext {
     fun shouldRegisterBadLambdaAndReturnError() {
         val computeContext = ComputeContext()
 
-        var registerResult0 = computeContext.register(ComputeLambda(1, "donut()"))
+        val registerResult0 = computeContext.register(ComputeLambda(1, "donut()"))
         when (registerResult0) {
             is Result.Success -> Assert.fail("Expected error not returned.")
             is Result.Failure -> Assert.assertNotEquals(0, registerResult0.error.code)
         }
 
-        var registerResult1 = computeContext.register(ComputeLambda(1, "Å^()["))
+        val registerResult1 = computeContext.register(ComputeLambda(1, "Å^()["))
         when (registerResult1) {
             is Result.Success -> Assert.fail("Expected error not returned.")
             is Result.Failure -> Assert.assertNotEquals(0, registerResult1.error.code)
