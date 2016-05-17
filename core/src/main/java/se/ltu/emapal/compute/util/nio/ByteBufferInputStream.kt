999503f6d6a572d8d460ff9f6483ed860b88361e
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-17 16:03:02 +0200

    Fix issues in ByteBufferInputStream

--- core/src/main/java/se/ltu/emapal/compute/util/nio/ByteBufferInputStream.kt
+++ core/src/main/java/se/ltu/emapal/compute/util/nio/ByteBufferInputStream.kt
@@ -8,12 +8,23 @@ import java.nio.ByteBuffer
  */
 class ByteBufferInputStream(val buffer: ByteBuffer) : InputStream() {
 
-    override fun read() = buffer.get().toInt()
+    override fun read(): Int {
+        val remaining = buffer.remaining()
+        if (remaining == 0) {
+            return -1
+        }
+        return buffer.get().toInt()
+    }
+
     override fun read(bytes: ByteArray): Int = read(bytes, 0, bytes.size)
 
     override fun read(bytes: ByteArray, offset: Int, length: Int): Int {
+        val remaining = buffer.remaining()
+        if (remaining == 0) {
+            return -1
+        }
         val position = buffer.position()
-        buffer.get(bytes, offset, length)
+        buffer.get(bytes, offset, Math.min(remaining, length))
         return buffer.position() - position
     }
 }
