71abbd43402e73690d6d8fc9dc5877ab42b49a16
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-19 09:44:43 +0200

    Fix spelling mistake in TestComputeChannel

--- core/src/test/java/se/ltu/emapal/compute/io/TestComputeChannel.kt
+++ core/src/test/java/se/ltu/emapal/compute/io/TestComputeChannel.kt
@@ -14,23 +14,23 @@ class TestComputeChannel {
     val channel = ComputeChannel(ByteBufferChannel(buffer))
 
     @Test
-    fun shouldTranscieveClientMessages() {
-        transcieve(ComputeMessage.ClientBatch(1, ComputeBatch(100, 200, "HELLO".toByteArray())))
-        transcieve(ComputeMessage.ClientError(2, ComputeError(1234, "Noes!")))
-        transcieve(ComputeMessage.ClientExit(3))
-        transcieve(ComputeMessage.ClientLogEntry(4, ComputeLogEntry(111, 222, "Surprise!")))
-        transcieve(ComputeMessage.ClientImAlive(5))
+    fun shouldTransceiveClientMessages() {
+        transceive(ComputeMessage.ClientBatch(1, ComputeBatch(100, 200, "HELLO".toByteArray())))
+        transceive(ComputeMessage.ClientError(2, ComputeError(1234, "Noes!")))
+        transceive(ComputeMessage.ClientExit(3))
+        transceive(ComputeMessage.ClientLogEntry(4, ComputeLogEntry(111, 222, "Surprise!")))
+        transceive(ComputeMessage.ClientImAlive(5))
     }
 
     @Test
-    fun shouldTranscieveServiceMessages() {
-        transcieve(ComputeMessage.ServiceBatch(1, ComputeBatch(111, 222, "hello".toByteArray())))
-        transcieve(ComputeMessage.ServiceExit(2))
-        transcieve(ComputeMessage.ServiceLambda(3, ComputeLambda(111, "lcm:register(lambda)")))
-        transcieve(ComputeMessage.ServiceImAlive(4))
+    fun shouldTransceiveServiceMessages() {
+        transceive(ComputeMessage.ServiceBatch(1, ComputeBatch(111, 222, "hello".toByteArray())))
+        transceive(ComputeMessage.ServiceExit(2))
+        transceive(ComputeMessage.ServiceLambda(3, ComputeLambda(111, "lcm:register(lambda)")))
+        transceive(ComputeMessage.ServiceImAlive(4))
     }
 
-    private fun transcieve(message: ComputeMessage) {
+    private fun transceive(message: ComputeMessage) {
         buffer.rewind()
         channel.write(message)
                 .unwrap()
