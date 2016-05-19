d333fb2f2e3e4d57992ed2a7414a874f55bd74bc
Emanuel Palm <emanuelpalm@gmail.com>
2016-05-19 09:50:25 +0200

    Add unimplemented Computer class

---
+++ core/src/main/java/se/ltu/emapal/compute/Computer.kt
@@ -0,0 +1,11 @@
+package se.ltu.emapal.compute
+
+/**
+ * A client computer.
+ *
+ * Receives compute instructions via some [ComputeClient], executes them using internally managed
+ * threads, and reports any results of relevance to a connected compute service.
+ */
+class Computer {
+    // TODO: Implement and integrate in Android app.
+}
